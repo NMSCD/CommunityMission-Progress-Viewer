37a7116a350ff44eb7ec7df0e4b01b45b16c3e78
Kurt Lourens <kurt.lourens@outlook.com>
2022-05-07 23:39:23 +0200

    0.6.6 - fix percentage

--- src/components/section/dataSection.tsx
+++ src/components/section/dataSection.tsx
@@ -70,16 +70,14 @@ export const DataSection: React.FC = () => {
                     displayValue={missionTierDisplay}
                     animate={false}
                 />
-                {
-                    missionPercentDisplay > 0 &&
-                    <DataSectionStat
-                        style="style5"
-                        iconClass="icon-database"
-                        description="Current Mission progress"
-                        value={missionPercentDisplay}
-                        displayValueFunc={(val) => val.toString() + ' %'}
-                    />
-                }
+                <DataSectionStat
+                    style="style5"
+                    iconClass="icon-database"
+                    description="Current Mission progress"
+                    value={missionPercentDisplay}
+                    displayValueFunc={(val) => val.toString() + ' %'}
+                    animate={false}
+                />
             </ul>
             <p className="content">The <AssistantNmsWebLink /> apps have a service that runs 24/7 and requests the status from the <GalacticAtlasWebLink />. This data is then stored both a database and within a Redis cache. This data is served to anyone who requests it from the <AssistantNmsApiLink />. This means that a request for the Community Mission progress through the <AssistantNmsApiLink /> never makes a request to HelloGames servers as the data is already sitting in the database or Redis cache. If the <AssistantNmsApiLink /> is being attacked through a DDOS attack, HelloGames will not be affected. We would never want to put strain on the HelloGames servers🔥</p>
         </section>

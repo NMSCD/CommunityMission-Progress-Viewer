e86833605d1206371bdab3e47451e734d7ae9075
Kurt Lourens <hi@kurtlourens.com>
2023-06-13 20:51:07 +0200

    🐛 Fix invalid dates | Closes #2

--- src/components/incidentReports.tsx
+++ src/components/incidentReports.tsx
@@ -9,8 +9,9 @@ interface IProps {
 
 export const IncidentReports: React.FC<IProps> = (props: IProps) => {
 
-    const defaultDateTextFromUtc = (dateString: string) => formatDate((new Date(dateString)).toLocaleString(), 'YYYY-MM-DD HH:mm (Z)');
-    const friendlyDateTextFromUtc = (dateString: string) => formatDate((new Date(dateString)).toLocaleString(), 'HH:mm on YYYY-MM-DD (Z)');
+    const shortDateTextFromUtc = (date: number) => formatDate((new Date(date)), 'YYYY-MM-DD');
+    const defaultDateTextFromUtc = (date: number) => formatDate((new Date(date)), 'YYYY-MM-DD HH:mm (Z)');
+    const friendlyDateTextFromUtc = (date: number) => formatDate((new Date(date)), 'HH:mm on YYYY-MM-DD (Z)');
 
     return (
         <>
@@ -28,7 +29,7 @@ export const IncidentReports: React.FC<IProps> = (props: IProps) => {
                     </div>
                     <div className="body-container">
                         <p className="body">The AssistantNMS tracker did not detect that Community Mission 77 had started for the Singularity Expedition. The data has also been altered for this period to reflect what was approximately displayed in game.</p>
-                        <p className="time">{formatDate((new Date('2023-06-07')).toLocaleString(), 'YYYY-MM-DD')} &nbsp; → &nbsp; {defaultDateTextFromUtc('2022-06-07T07:57Z')}</p>
+                        <p className="time">{shortDateTextFromUtc(Date.UTC(2023, 6, 7))} &nbsp; → &nbsp; {defaultDateTextFromUtc(Date.UTC(2023, 6, 7, 7, 57))}</p>
                     </div>
                 </div>
                 <div className="report">
@@ -40,7 +41,7 @@ export const IncidentReports: React.FC<IProps> = (props: IProps) => {
                     </div>
                     <div className="body-container">
                         <p className="body">The AssistantNMS tracker did not detect that Community Mission 71 had already started for several days. Extra logic has been added to try prevent this issue occurring again. The data has also been altered for this period to reflect what was approximately displayed in game.</p>
-                        <p className="time">{formatDate((new Date('2023-01-20')).toLocaleString(), 'YYYY-MM-DD')} &nbsp; → &nbsp; {formatDate((new Date('2023-01-23')).toLocaleString(), 'YYYY-MM-DD')}</p>
+                        <p className="time">{shortDateTextFromUtc(Date.UTC(2023, 1, 20))} &nbsp; → &nbsp; {shortDateTextFromUtc(Date.UTC(2023, 1, 23))}</p>
                     </div>
                 </div>
                 <div className="report">
@@ -51,8 +52,8 @@ export const IncidentReports: React.FC<IProps> = (props: IProps) => {
                         </div>
                     </div>
                     <div className="body-container">
-                        <p className="body">A large amount of progress was mysteriously made at around {friendlyDateTextFromUtc('2022-08-05T08:57Z')}. The cause of this sudden spike in progress has not been officially been commented on.</p>
-                        <p className="time">{defaultDateTextFromUtc('2022-08-05T08:57Z')}</p>
+                        <p className="body">A large amount of progress was mysteriously made at around {friendlyDateTextFromUtc(Date.UTC(2022, 8, 5, 8, 57))}. The cause of this sudden spike in progress has not been officially been commented on.</p>
+                        <p className="time">{defaultDateTextFromUtc(Date.UTC(2022, 8, 5, 8, 57))}</p>
                     </div>
                 </div>
                 <div className="report">
@@ -63,8 +64,8 @@ export const IncidentReports: React.FC<IProps> = (props: IProps) => {
                         </div>
                     </div>
                     <div className="body-container">
-                        <p className="body">First noticed at {friendlyDateTextFromUtc('2022-07-24T21:55Z')} by <BasicLink href="https://twitter.com/bomber_that/status/1551433207387750405" title="ThatBomberBoi">ThatBomberBoi on Twitter</BasicLink>. The <BasicLink href="https://nomanssky.com">website</BasicLink> was confirmed to be down.</p>
-                        <p className="time">{defaultDateTextFromUtc('2022-07-24T21:55Z')} &nbsp; → &nbsp; {defaultDateTextFromUtc('2022-07-24T22:10Z')}</p>
+                        <p className="body">First noticed at {friendlyDateTextFromUtc(Date.UTC(2022, 7, 24, 21, 55))} by <BasicLink href="https://twitter.com/bomber_that/status/1551433207387750405" title="ThatBomberBoi">ThatBomberBoi on Twitter</BasicLink>. The <BasicLink href="https://nomanssky.com">website</BasicLink> was confirmed to be down.</p>
+                        <p className="time">{defaultDateTextFromUtc(Date.UTC(2022, 7, 24, 21, 55))} &nbsp; → &nbsp; {defaultDateTextFromUtc(Date.UTC(2022, 7, 24, 22, 10))}</p>
                     </div>
                 </div>
                 <div className="report">
@@ -75,8 +76,8 @@ export const IncidentReports: React.FC<IProps> = (props: IProps) => {
                         </div>
                     </div>
                     <div className="body-container">
-                        <p className="body">First noticed at {friendlyDateTextFromUtc('2022-06-03T09:54Z')} by <BasicLink href='https://twitter.com/ProceduralTRV/status/1532661985279631360' title='ProceduralTraveller'>Procedural Traveller on Twitter</BasicLink>. The <BasicLink href="https://nomanssky.com">website</BasicLink> was confirmed to be down by multiple players.</p>
-                        <p className="time">{defaultDateTextFromUtc('2022-06-03T09:54Z')} &nbsp; → &nbsp; {defaultDateTextFromUtc('2022-06-06T09:31Z')}</p>
+                        <p className="body">First noticed at {friendlyDateTextFromUtc(Date.UTC(2022, 6, 3, 9, 54))} by <BasicLink href='https://twitter.com/ProceduralTRV/status/1532661985279631360' title='ProceduralTraveller'>Procedural Traveller on Twitter</BasicLink>. The <BasicLink href="https://nomanssky.com">website</BasicLink> was confirmed to be down by multiple players.</p>
+                        <p className="time">{defaultDateTextFromUtc(Date.UTC(2022, 6, 3, 9, 54))} &nbsp; → &nbsp; {defaultDateTextFromUtc(Date.UTC(2022, 6, 6, 9, 31))}</p>
                     </div>
                 </div>
                 <div className="report">
@@ -87,8 +88,8 @@ export const IncidentReports: React.FC<IProps> = (props: IProps) => {
                         </div>
                     </div>
                     <div className="body-container">
-                        <p className="body">First detected at around {friendlyDateTextFromUtc('2022-05-30T23:00Z')}, the Galactic Atlas API (the service which reports the progress to the Galactic Atlas website) stopped responding to Assistant for No Man's Sky trackers. The issue seems to have been resolved by HelloGames and the problem Tier was skipped.</p>
-                        <p className="time">{defaultDateTextFromUtc('2022-05-30T09:00Z')} &nbsp; → &nbsp; {defaultDateTextFromUtc('2022-05-30T23:00Z')}</p>
+                        <p className="body">First detected at around {friendlyDateTextFromUtc(Date.UTC(2022, 5, 30, 23, 0))}, the Galactic Atlas API (the service which reports the progress to the Galactic Atlas website) stopped responding to Assistant for No Man's Sky trackers. The issue seems to have been resolved by HelloGames and the problem Tier was skipped.</p>
+                        <p className="time">{defaultDateTextFromUtc(Date.UTC(2022, 5, 30, 9, 0))} &nbsp; → &nbsp; {defaultDateTextFromUtc(Date.UTC(2022, 5, 30, 23, 0))}</p>
                     </div>
                 </div>
                 <div className="report">
@@ -99,8 +100,8 @@ export const IncidentReports: React.FC<IProps> = (props: IProps) => {
                         </div>
                     </div>
                     <div className="body-container">
-                        <p className="body">First detected at around {friendlyDateTextFromUtc('2022-05-30T09:00Z')}, the Community Mission progress percentage started decreasing for the first time ever recorded. This caused a lot of confusion in the NMS community and lead to a lot of speculation.</p>
-                        <p className="time">{defaultDateTextFromUtc('2022-05-30T09:00Z')} &nbsp; → &nbsp; {defaultDateTextFromUtc('2022-05-30T23:00Z')}</p>
+                        <p className="body">First detected at around {friendlyDateTextFromUtc(Date.UTC(2022, 5, 30, 9, 0))}, the Community Mission progress percentage started decreasing for the first time ever recorded. This caused a lot of confusion in the NMS community and lead to a lot of speculation.</p>
+                        <p className="time">{defaultDateTextFromUtc(Date.UTC(2022, 5, 30, 9, 0))} &nbsp; → &nbsp; {defaultDateTextFromUtc(Date.UTC(2022, 5, 30, 23, 0))}</p>
                     </div>
                 </div>
                 <div className="report">
@@ -111,8 +112,8 @@ export const IncidentReports: React.FC<IProps> = (props: IProps) => {
                         </div>
                     </div>
                     <div className="body-container">
-                        <p className="body">A large amount of progress was mysteriously made at around {friendlyDateTextFromUtc('2022-04-30T16:00Z')}. The cause of this sudden spike in progress has not been officially been commented on.</p>
-                        <p className="time">{defaultDateTextFromUtc('2022-04-30T16:00Z')}</p>
+                        <p className="body">A large amount of progress was mysteriously made at around {friendlyDateTextFromUtc(Date.UTC(2022, 4, 30, 16, 0))}. The cause of this sudden spike in progress has not been officially been commented on.</p>
+                        <p className="time">{defaultDateTextFromUtc(Date.UTC(2022, 4, 30, 16, 0))}</p>
                     </div>
                 </div>
                 <div className="report">
@@ -123,8 +124,8 @@ export const IncidentReports: React.FC<IProps> = (props: IProps) => {
                         </div>
                     </div>
                     <div className="body-container">
-                        <p className="body">A large amount of progress was mysteriously made at around {friendlyDateTextFromUtc('2022-04-04T09:00Z')}. The cause of this sudden spike in progress has not been officially been commented on.</p>
-                        <p className="time">{defaultDateTextFromUtc('2022-04-04T09:00Z')}</p>
+                        <p className="body">A large amount of progress was mysteriously made at around {friendlyDateTextFromUtc(Date.UTC(2022, 4, 4, 9, 0))}. The cause of this sudden spike in progress has not been officially been commented on.</p>
+                        <p className="time">{defaultDateTextFromUtc(Date.UTC(2022, 4, 4, 9, 0))}</p>
                     </div>
                 </div>
             </div>
